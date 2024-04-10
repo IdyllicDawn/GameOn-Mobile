b586cd6ad91b5d2059d49865119e2a25423030f2
HaniLabs <110095889+HaniLabs@users.noreply.github.com>
2024-04-10 16:04:47 -0400

    typeingGameApi+EndScreen

--- lib/typeracer_game.dart
+++ lib/typeracer_game.dart
@@ -1,31 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'typeracer_score.dart';
 
-class TypingTestScreen extends StatelessWidget {
+class TypingTestScreen extends StatefulWidget {
   final String? loggedInUsername;
 
-  const TypingTestScreen({super.key, required this.loggedInUsername});
+  const TypingTestScreen({super.key, this.loggedInUsername});
+
+  @override
+  _TypingTestScreenState createState() => _TypingTestScreenState();
+}
+
+class _TypingTestScreenState extends State<TypingTestScreen> {
+  WebViewController? _webViewController;
+  String message = "";
+
+  @override
+  void initState() {
+    // WebView controller initialization and setup
+    _webViewController = WebViewController()
+      ..loadFlutterAsset("assets/web/typingTest.html")
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..runJavaScript('document.querySelector("input").focus();')
+      ..addJavaScriptChannel(
+        "messageHandler",
+        onMessageReceived: (JavaScriptMessage javaScriptMessage) {
+          print("Message from the web view=\"${javaScriptMessage.message}\"");
+          //if the message is WhatUsername, pass the username to the HTML
+          //if logged in username is not null, pass it to the HTML
+          if (javaScriptMessage.message == "WhatUsername?" &&
+              widget.loggedInUsername != null) {
+            passUsernameToHTML();
+          } else {
+            setState(() {
+              message = javaScriptMessage.message;
+            });
+            //if the message containes "score" parse the data into respective variables
+            //example message :"Score,WPM,Accuracy: score,wpm,accuracy"
+            if (message.contains("Score,WPM,Accuracy:")) {
+              print("Score received");
+              List<String> scoreData = message.split(":")[1].split(",");
+              int score = int.parse(scoreData[0]);
+              print("Score: $score");
+              double wpm = double.parse(scoreData[1]);
+              print("WPM: $wpm");
+              double accuracy = double.parse(scoreData[2]);
+              print("score: $score, wpm: $wpm, accuracy: $accuracy");
+              //switch to the TypingScoreScreen with the score data
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => TypingScoreScreen(
+                    loggedInUsername: widget.loggedInUsername,
+                    score: score.toInt(),
+                    wpm: wpm.toDouble(),
+                    accuracy: accuracy.toDouble(),
+                  ),
+                ),
+              );
+            }
+          }
+        },
+      );
+
+    super.initState();
+  }
+
+  Widget buildWebView() {
+    return WebViewWidget(
+      controller: _webViewController!,
+    );
+  }
+
+  void passUsernameToHTML() {
+    _webViewController?.runJavaScript(
+        'receiveMessageFromFlutter("${widget.loggedInUsername}");');
+  }
 
   @override
   Widget build(BuildContext context) {
-    final controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..enableZoom(false)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..loadFlutterAsset('assets/web/typingTest.html');
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('TypeRacer'),
+        title: const Text("Typing Test"),
       ),
-      body: GestureDetector(
-        onTap: () {
-          controller.runJavaScript(
-              'var inputElement = document.getElementById("user-input");inputElement.focus();');
-          //set username to LoggedInUsername using javascript updateUsername('CoolName?');
-          controller.runJavaScript('updateUsername("$loggedInUsername");');
-        },
-        child: WebViewWidget(controller: controller),
+      body: Column(
+        children: [
+          Expanded(child: buildWebView()),
+        ],
       ),
     );
   }
---
+++ lib/typeracer_score.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+import 'package:GameOn/main_page.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'typeracer_game.dart';
+import 'main_page.dart';
+
+class TypingScoreScreen extends StatefulWidget {
+  final String? loggedInUsername;
+  final int? score;
+  final double? wpm;
+  final double? accuracy;
+
+  const TypingScoreScreen(
+      {super.key, this.loggedInUsername, this.score, this.wpm, this.accuracy});
+
+  @override
+  TypingScoreState createState() => TypingScoreState();
+}
+
+class TypingScoreState extends State<TypingScoreScreen> {
+  @override
+  void initState() {
+    super.initState();
+    if (widget.loggedInUsername != null) {
+      sendTypingScore(
+          widget.loggedInUsername, widget.score, widget.wpm!.toInt());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Typing Test Score"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Score: ${widget.score.toString()}"),
+            Text("WPM: ${widget.wpm}"),
+            Text("Accuracy: ${widget.accuracy}"),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TypingTestScreen(
+                      loggedInUsername: widget.loggedInUsername,
+                    ),
+                  ),
+                );
+              },
+              child: const Text("Play Again"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(
+                      loggedInUsername: widget.loggedInUsername,
+                    ),
+                  ),
+                );
+              },
+              child: const Text("Leaderboard"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> sendTypingScore(String? username, int? score, int? speed) async {
+    final url = Uri.parse(
+        'https://group8large-57cfa8808431.herokuapp.com/api/addTypingData');
+    final response = await http.post(
+      url,
+      body: jsonEncode({
+        'username': username,
+        'score': score,
+        'speed': speed,
+        'date': DateTime.now().toString(),
+        'device': 'Phone'
+      }),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      print("Success");
+    }
+  }
+}
